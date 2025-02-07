e3b33b32e24443b0f7e6436c3afd7c9d75eb3657
iagorv <Iagorive18@gmail.com>
2025-02-08 00:35:48 +0100

    fix(ScriptNotas.sql): cambiar long por bigint

--- ScriptNotas.sql
+++ ScriptNotas.sql
@@ -3,7 +3,7 @@ create database if not exists NotasDB;
 use NotasDB;
 
 create table Usuarios(
-	id long primary key auto_increment,
+	id BIGINT primary key auto_increment,
     email varchar(50) not null,
     nombre varchar(20) not null,
     apellidos varchar(40) not null,
@@ -12,30 +12,30 @@ create table Usuarios(
 );
 
 create table Escritorios(
-	id long primary key auto_increment,
+	id BIGINT primary key auto_increment,
     nombre varchar(20) not null,
-    idUsuario long not null,
+    idUsuario BIGINT not null,
     
     foreign key Escritorios(idUsuario) references Usuarios(id)
 );
 
 
 create table Categorias(
-	id long primary key auto_increment,
+	id BIGINT primary key auto_increment,
     color varchar(6) not null,
     nombre varchar(15) not null,
-    idUsuario long not null,
+    idUsuario BIGINT not null,
     
     foreign key (idUsuario) references Usuarios(id)
 );
 create table Notas(
-	id long primary key auto_increment,
+	id BIGINT primary key auto_increment,
 	titulo varchar(30) not null,
     descripcion mediumtext,
     color varchar(6) not null,
-    idCreador long not null,
-    idEscritorio long not null,
-    idCategoria long not null,
+    idCreador BIGINT not null,
+    idEscritorio BIGINT not null,
+    idCategoria BIGINT not null,
     foreign key (idCreador) references Usuarios(id),
      foreign key (idCategoria) references Categorias(id),
     foreign key (idEscritorio) references Escritorios(id)
@@ -46,19 +46,18 @@ create table Preferencias(
     modoOscuro boolean default false not null,    
     tamañoFuente tinyint not null,
     idioma varchar(2) not null,
-    idUsuario long not null primary key,
+    idUsuario BIGINT not null primary key,
     
 	foreign key (idUsuario) references Usuarios(id)
 );
 
 create table Usuarios_Notas(
-	idUsuario long not null,
-    idNota long not null,
+	idUsuario BIGINT not null,
+    idNota BIGINT not null,
     
 	foreign key (idUsuario) references Usuarios(id),
 	foreign key (idNota) references Notas(id),
     primary key(idUsuario, idNota)
 );
 
-insert into Usuarios (email, nombre, apellidos, contraseña) values
-    ("admin@gmail.com","Administrador","Admin","123");
+insert into Usuarios(email, nombre, apellidos, contraseña) values ("admin@gmail.com","Administrador","Admin","123");
