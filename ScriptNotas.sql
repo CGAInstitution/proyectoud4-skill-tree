5d41328f292721690e15d0d6c2aa79590fe927e4
iagorv <Iagorive18@gmail.com>
2025-02-10 17:13:42 +0100

    feat(): Ahora las notas tienen campo posicion y aparecen donde deben

--- ScriptNotas.sql
+++ ScriptNotas.sql
@@ -33,6 +33,8 @@ create table Notas(
 	titulo varchar(30) not null,
     descripcion mediumtext,
     color varchar(6) not null,
+    posicionX int not null,
+     posiciony int not null,
     id_Creador BIGINT not null,
     id_Escritorio BIGINT not null,
     idCategoria BIGINT not null,
@@ -68,6 +70,6 @@ insert into Escritorios(nombre, id_Usuario) values ("Escritorio1",1);
 insert into Escritorios(nombre, id_Usuario) values ("Escritorio2",1);
 insert into Categorias(color,nombre,idUsuario) values ("43e24f","importante",1);
 
-insert into Notas(titulo,descripcion,color,id_Creador,id_Escritorio,idCategoria) values ("Nota 1"," Esta es mi primera nota","43e24f",1,1,1);
-insert into Notas(titulo,descripcion,color,id_Creador,id_Escritorio,idCategoria) values ("Nota 2"," Esta es mi segunda nota","43224f",1,1,1);
-insert into Notas(titulo,descripcion,color,id_Creador,id_Escritorio,idCategoria) values ("Nota 3"," Esta es mi tercera nota","f3e28f",1,2,1);
+insert into Notas(titulo,descripcion,color,id_Creador,id_Escritorio,idCategoria,posicionX,posicionY) values ("Nota 1"," Esta es mi primera nota","43e24f",1,1,1,1000,450);
+insert into Notas(titulo,descripcion,color,id_Creador,id_Escritorio,idCategoria,posicionX,posicionY) values ("Nota 2"," Esta es mi segunda nota","43224f",1,1,1,50,200);
+insert into Notas(titulo,descripcion,color,id_Creador,id_Escritorio,idCategoria,posicionX,posicionY) values ("Nota 3"," Esta es mi tercera nota","f3e28f",1,2,1,400,200);
