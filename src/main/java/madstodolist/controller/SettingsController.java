69207e4a97f32a0359f786919eab44746536f984
Samugcor <samueligcorp@gmail.com>
2025-02-17 19:39:02 +0100

    feat: el usuario puede cambiar sus datos desde la opción de ajutes

--- src/main/java/madstodolist/controller/SettingsController.java
+++ src/main/java/madstodolist/controller/SettingsController.java
@@ -1,15 +1,20 @@
 package madstodolist.controller;
 
 import madstodolist.authentication.ManagerUserSession;
+import madstodolist.dto.SettingsForm;
 import madstodolist.dto.UsuarioData;
 import madstodolist.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.util.Map;
 
 @Controller
@@ -33,8 +38,20 @@ public class SettingsController {
     }
 
     @PostMapping("/user/settings/savesettings")
-    public String guardarSettings(@RequestBody Map<String, Object> requestData) {
-        requestData.get()
+    public String guardarSettings(@Valid @ModelAttribute SettingsForm settingsForm, BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes) {
+        if(bindingResult.hasErrors()) {
+            UsuarioData userData = usuarioService.findById(managerUserSession.usuarioLogeado());
+            model.addAttribute("usuario", userData);
+            return "formSettings";
+        }
+        usuarioService.actualizarNombre(managerUserSession.usuarioLogeado(), settingsForm.getNombre());
+        usuarioService.actualizarApellido(managerUserSession.usuarioLogeado(), settingsForm.getApellidos());
+        usuarioService.actualizarEmail(managerUserSession.usuarioLogeado(), settingsForm.getEmail());
+        if (settingsForm.getContrasenia() != null) {
+            usuarioService.actualizarContrasenia(managerUserSession.usuarioLogeado(), settingsForm.getContrasenia());
+
+        }
+        redirectAttributes.addFlashAttribute("show", true);
         return "redirect:/user/settings";
     }
 }
