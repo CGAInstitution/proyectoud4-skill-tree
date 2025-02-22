62311a69f7f486fbcce8bd39a65ff18318c0d061
Manue14 <115022266+Manue14@users.noreply.github.com>
2025-02-22 19:56:41 +0100

    hotfix(Usuario.java)
    Cambiar el nombre de la tabla contrasena por contraseña.

--- src/main/java/madstodolist/model/Usuario.java
+++ src/main/java/madstodolist/model/Usuario.java
@@ -34,7 +34,7 @@ public class Usuario {
 
     @Size(max = 255)
     @NotNull
-    @Column(name = "`contrasena`", nullable = false)
+    @Column(name = "`contraseña`", nullable = false)
     private String contraseña;
 
     @OneToMany(mappedBy = "idUsuario")
